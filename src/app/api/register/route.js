459f67988676f4c3927ab61fc5c973220d0b7ba2
sbautista00 <sjbautista@line.com.ph>
2024-03-15 15:54:08 +0800

    friday accomplishments

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,3 +1,6 @@
-export function POST(req){
-    
+export async function POST(req) {
+  const body = await req.json();
+  mongoose.connect(process.env.MONGO_URL);
+  const createdUser = await User.create(body);
+  return Response.json(createdUser);
 }
