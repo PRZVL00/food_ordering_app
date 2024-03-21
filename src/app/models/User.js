be5485414f2b77e8dd2946fdbca622c76f1de8a3
sbautista00 <sjbautista@line.com.ph>
2024-03-21 15:43:10 +0800

    3-21

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,10 +1,11 @@
+import { Schema, model, models } from "mongoose";
+
 const UserSchema = new Schema(
   {
     email: { type: String, required: true, unique: true },
     password: {
       type: String,
       required: true,
-      unique: true,
       validate: (pass) => {
         if (!pass?.length || pass.length < 5) {
           new Error("Invalid password");
